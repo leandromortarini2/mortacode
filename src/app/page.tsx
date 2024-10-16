97a1951890f2ac1f739110a0d7b490841ef02f8f
leandro mortarini <leandromortarini2@gmail.com>
2024-10-16 17:38:52 -0300

    importacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import { NavBar } from "@/components/Header/NavBar";
-import { Hero } from "./view/home/hero";
 import { About } from "./view/about/About";
 import { Projects } from "./view/Projects/Projects";
 import { MobileMenu } from "@/components/Header/MobileMenu";
+import { Hero } from "./view/home/hero";
 
 export default function Home() {
   return (
