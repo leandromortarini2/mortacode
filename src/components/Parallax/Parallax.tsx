6747ac3ca194272b4bd98c303dd842800fc6ca38
leandro mortarini <leandromortarini2@gmail.com>
2024-10-19 21:13:10 -0300

    comit

--- src/components/Parallax/Parallax.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React from "react";
-import { HeroParallax } from "../ui/hero-parallax";
-import { proyectos } from "@/lib/projectsArray";
-export function Parallax() {
-  return <HeroParallax products={proyectos} />;
-}
