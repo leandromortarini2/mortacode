3f3a51f25436e1588238ed9d9b0a4f17248eeeae
leandro mortarini <leandromortarini2@gmail.com>
2024-10-21 20:29:45 -0300

    borrando hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,9 +21,7 @@ export default function Home() {
       </div>
       <NavBar />
       <MobileMenu />
-      {/* <Hero /> */}
       <MainHero />
-      {/* <SuperHero /> */}
       <About />
       <Projects />
     </>
