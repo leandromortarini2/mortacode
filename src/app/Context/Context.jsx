06b1236149b9eeae1d6e6349cd2c0c19d3292cf1
leandro mortarini <leandromortarini2@gmail.com>
2024-10-22 20:56:23 -0300

    local storage

--- src/app/Context/Context.jsx
+++ src/app/Context/Context.jsx
@@ -7,7 +7,7 @@ export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
   const toggleTheme = () => {
-    const newTheme = theme === "dark" ? "light" : "dark";
+    const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
     localStorage.setItem("theme", newTheme);
   };
