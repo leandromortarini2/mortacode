6db628dda6f4383e33876261350b1f1d87a4642b
leandro mortarini <leandromortarini2@gmail.com>
2024-10-22 21:10:51 -0300

    gano el modo claro

--- src/app/Context/Context.jsx
+++ src/app/Context/Context.jsx
@@ -4,12 +4,11 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [theme, setTheme] = useState("light");
 
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
   };
 
   useEffect(() => {
